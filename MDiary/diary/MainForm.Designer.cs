94503c4b6d01617c5536ee5410e9be92155b417f
walogia <walogia211@sina.com>
2019-01-27 11:13:40 +0800

    提交文件

---
+++ MDiary/diary/MainForm.Designer.cs
@@ -0,0 +1,291 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2019/1/25 星期五
+ * 时间: 15:12
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+namespace diary
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton toolStripButton1;
+		private System.Windows.Forms.ToolStripButton toolStripButton2;
+		private System.Windows.Forms.ToolStripButton toolStripButton3;
+		private System.Windows.Forms.ToolStripButton toolStripButton4;
+		private System.Windows.Forms.ToolStripButton toolStripButton5;
+		private System.Windows.Forms.ToolStripButton toolStripButton6;
+		private System.Windows.Forms.ToolStripButton toolStripButton7;
+		private System.Windows.Forms.ToolStripButton toolStripButton8;
+		private System.Windows.Forms.ToolStripButton toolStripButton9;
+		private System.Windows.Forms.ToolStripButton toolStripButton10;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton9 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton10 = new System.Windows.Forms.ToolStripButton();
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(3, 28);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(275, 203);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(4, 237);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(48, 25);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "日期";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(178, 238);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(48, 25);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "图片";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button2MouseDown);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(4, 296);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox2.Size = new System.Drawing.Size(275, 104);
+			this.textBox2.TabIndex = 3;
+			// 
+			// textBox3
+			// 
+			this.textBox3.AllowDrop = true;
+			this.textBox3.Location = new System.Drawing.Point(58, 237);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(114, 26);
+			this.textBox3.TabIndex = 4;
+			this.textBox3.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBox3DragDrop);
+			this.textBox3.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBox3DragEnter);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(232, 237);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(48, 25);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "链接";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.toolStripButton1,
+			this.toolStripButton2,
+			this.toolStripButton3,
+			this.toolStripButton4,
+			this.toolStripButton5,
+			this.toolStripButton6,
+			this.toolStripButton7,
+			this.toolStripButton8,
+			this.toolStripButton9,
+			this.toolStripButton10});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(282, 27);
+			this.toolStrip1.TabIndex = 6;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton1.Text = "toolStripButton1";
+			this.toolStripButton1.ToolTipText = "打开";
+			this.toolStripButton1.Click += new System.EventHandler(this.ToolStripButton1Click);
+			// 
+			// toolStripButton2
+			// 
+			this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+			this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton2.Name = "toolStripButton2";
+			this.toolStripButton2.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton2.Text = "toolStripButton2";
+			this.toolStripButton2.ToolTipText = "保存";
+			this.toolStripButton2.Click += new System.EventHandler(this.ToolStripButton2Click);
+			// 
+			// toolStripButton3
+			// 
+			this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+			this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton3.Name = "toolStripButton3";
+			this.toolStripButton3.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton3.Text = "toolStripButton3";
+			this.toolStripButton3.ToolTipText = "加粗";
+			this.toolStripButton3.Click += new System.EventHandler(this.ToolStripButton3Click);
+			// 
+			// toolStripButton4
+			// 
+			this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+			this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton4.Name = "toolStripButton4";
+			this.toolStripButton4.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton4.Text = "toolStripButton4";
+			this.toolStripButton4.ToolTipText = "斜体";
+			this.toolStripButton4.Click += new System.EventHandler(this.ToolStripButton4Click);
+			// 
+			// toolStripButton5
+			// 
+			this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+			this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton5.Name = "toolStripButton5";
+			this.toolStripButton5.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton5.Text = "toolStripButton5";
+			this.toolStripButton5.ToolTipText = "居中";
+			this.toolStripButton5.Click += new System.EventHandler(this.ToolStripButton5Click);
+			// 
+			// toolStripButton6
+			// 
+			this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+			this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton6.Name = "toolStripButton6";
+			this.toolStripButton6.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton6.Text = "toolStripButton6";
+			this.toolStripButton6.ToolTipText = "颜色";
+			this.toolStripButton6.Click += new System.EventHandler(this.ToolStripButton6Click);
+			// 
+			// toolStripButton7
+			// 
+			this.toolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton7.Image")));
+			this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton7.Name = "toolStripButton7";
+			this.toolStripButton7.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton7.Text = "toolStripButton7";
+			this.toolStripButton7.ToolTipText = "列表";
+			this.toolStripButton7.Click += new System.EventHandler(this.ToolStripButton7Click);
+			// 
+			// toolStripButton8
+			// 
+			this.toolStripButton8.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton8.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton8.Image")));
+			this.toolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton8.Name = "toolStripButton8";
+			this.toolStripButton8.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton8.Text = "toolStripButton8";
+			this.toolStripButton8.ToolTipText = "MaHua 在线markdown编辑器";
+			this.toolStripButton8.Click += new System.EventHandler(this.ToolStripButton8Click);
+			// 
+			// toolStripButton9
+			// 
+			this.toolStripButton9.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton9.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton9.Image")));
+			this.toolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton9.Name = "toolStripButton9";
+			this.toolStripButton9.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton9.Text = "toolStripButton9";
+			this.toolStripButton9.ToolTipText = "缩进";
+			this.toolStripButton9.Click += new System.EventHandler(this.ToolStripButton9Click);
+			// 
+			// toolStripButton10
+			// 
+			this.toolStripButton10.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton10.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton10.Image")));
+			this.toolStripButton10.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton10.Name = "toolStripButton10";
+			this.toolStripButton10.Size = new System.Drawing.Size(24, 24);
+			this.toolStripButton10.Text = "toolStripButton10";
+			this.toolStripButton10.ToolTipText = "换行";
+			this.toolStripButton10.Click += new System.EventHandler(this.ToolStripButton10Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(282, 266);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "MDiary";
+			this.TopMost = true;
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MDiary/diary/MainForm.cs
@@ -0,0 +1,229 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2019/1/25 星期五
+ * 时间: 15:12
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace diary
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void ToolStripButton1Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Filter = "MarkDown文件|*.MD|文本文件|*.txt|所有文件|*.*";
+			ofd.ShowDialog();
+			string path = ofd.FileName;
+			if (path == ""){return;}
+			using (FileStream fsRead = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read))
+			{
+			byte[] buffer = new byte[1024 * 1024 * 5];
+			int r = fsRead.Read(buffer, 0, buffer.Length);
+			textBox1.Text = Encoding.Default.GetString(buffer, 0, r);
+			}
+		}
+		
+		void ToolStripButton2Click(object sender, EventArgs e)
+		{
+			try {
+				SaveFileDialog sfd = new SaveFileDialog();
+				sfd.Filter = "MarkDown文件|*.MD|文本文件|*.txt|所有文件|*.*";
+				sfd.FileName= DateTime.Now.ToString("yyyy-MM-dd"); 
+				sfd.ShowDialog();
+				string path = sfd.FileName;
+				if (path == ""){return;}
+				using (FileStream fsWrite = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+				{
+					byte[] buffer = Encoding.Default.GetBytes(textBox1.Text+"\r\n\r\n\r\n\r\n\r\n\r\n"+textBox2.Text);
+					fsWrite.Write(buffer, 0, buffer.Length);
+				}
+			} catch (Exception) {
+			}
+				
+		}		
+		void ToolStripButton3Click(object sender, EventArgs e)
+		{
+			textBox1.Text+="**粗体**"+"\r\n";
+		}
+		void ToolStripButton4Click(object sender, EventArgs e)
+		{
+			textBox1.Text+="*斜体*"+"\r\n";
+		}
+		
+		void ToolStripButton5Click(object sender, EventArgs e)
+		{
+			textBox1.Text+="<p  align=\"center\">居中</p>"+"\r\n";
+		}
+		void ToolStripButton6Click(object sender, EventArgs e)
+		{
+			textBox1.Text+="<font color=\"violet\"> </font>"+"\r\n";
+		}
+							
+		void ToolStripButton7Click(object sender, EventArgs e)
+		{
+			 textBox1.Text+="1.  "+"\r\n"+"2. "+"\r\n"+"3. "+"\r\n"+"4. "+"\r\n";
+		}
+		
+		void ToolStripButton8Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start("http://mahua.jser.me/"); 
+		}
+		void ToolStripButton9Click(object sender, EventArgs e)
+		{
+			textBox1.Text+="&nbsp;&nbsp;"+"\r\n";
+		}
+		void ToolStripButton10Click(object sender, EventArgs e)
+		{
+			textBox1.Text+="<br/>"+"\r\n";
+		}
+
+		
+		//-----------------------------------------------------//		
+		void Button1Click(object sender, EventArgs e)
+		{
+			textBox1.Text+="*"+DateTime.Now.ToString()+"*"+"\r\n";    
+		}
+
+		void Button2MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				if(textBox3.Text !=""){
+				string img="![]["+GetRandomString(6)+"]";
+				textBox1.Text+=img+"\r\n";
+				
+				string base64="["+GetRandomString(6)+"]:data:image/png;base64,";
+				textBox2.Text+=base64+ImgToBase64String(textBox3.Text.Trim())+"\r\n";
+				}
+			}
+			else if(e.Button == MouseButtons.Right)
+			{
+			 if(textBox3.Text !=""){
+				string img="![]["+GetRandomString(6)+"]";
+				string base64="["+GetRandomString(6)+"]:data:image/png;base64,";
+				string clip=img+"\r\n"+base64+ImgToBase64String(textBox3.Text.Trim())+"\r\n";
+				 Clipboard.SetDataObject(clip); 
+				}
+			}
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			if(textBox3.Text !=""){
+				string link="[链接]("+textBox3.Text.Trim()+")";
+				textBox1.Text+=link+"\r\n";
+			}
+	
+		}
+				
+		void TextBox1KeyPress(object sender, KeyPressEventArgs e)
+		{
+	   		 TextBox textBox = sender as TextBox;
+		    if (textBox == null)
+		        return;
+		    if (e.KeyChar == (char)1)       // Ctrl-A 相当于输入了AscII=1的控制字符
+		    {
+		        textBox.SelectAll();
+		        e.Handled = true;      // 不再发出“噔”的声音
+		    }
+		}
+				
+		void TextBox3DragDrop(object sender, DragEventArgs e)
+		{
+	        string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString(); 
+	      	 System.IO.FileInfo fileInfo = new System.IO.FileInfo(path);
+	      	 if(System.Math.Ceiling(fileInfo.Length / 1024.0) >100)
+	      	 {MessageBox.Show("请保持图片大小低于100KB");}
+	      	 else
+	      	 {textBox3.Text = path;}
+		}
+		void TextBox3DragEnter(object sender, DragEventArgs e)
+		{
+	            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;   //重要代码：表明是所有类型的数据，比如文件路径
+           	    else
+                e.Effect = DragDropEffects.None;
+		}
+		
+	
+		//------------------------------函数------------------------//
+		/// <summary>
+		/// 图片转为base64编码的字符串
+		/// </summary>
+		/// <param name="Imagefilename"></param>
+		/// <returns></returns>
+		public string ImgToBase64String(string Imagefilename)
+		{
+		try
+		{
+		Bitmap bmp = new Bitmap(Imagefilename);
+		
+		MemoryStream ms = new MemoryStream();
+		bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+		byte[] arr = new byte[ms.Length];
+		ms.Position = 0;
+		ms.Read(arr, 0, (int)ms.Length);
+		ms.Close();
+		return Convert.ToBase64String(arr);
+		}
+		catch (Exception)
+		{
+		return null;
+		}
+		}
+
+		 string GetRandomString(int count)
+        {
+            int number;
+            string checkCode = String.Empty;     //存放随机码的字符串 
+
+            System.Random random = new Random();
+
+            for (int i = 0; i < count; i++) //产生4位校验码 
+            {
+                number = random.Next();
+                number = number % 36;
+                if (number < 10)
+                {
+                    number += 48;    //数字0-9编码在48-57 
+                }
+                else
+                {
+                    number += 55;    //字母A-Z编码在65-90 
+                }
+
+                checkCode += ((char)number).ToString();
+            }
+            return checkCode;
+        }
+
+
+
+
+
+		//**********************************************************//
+	}
+}
